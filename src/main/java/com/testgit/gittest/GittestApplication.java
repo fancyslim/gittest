72a8d2ea17602548eef739dc4b2f28528db3f44d
wufan <729627242@qq.com>
2019-08-05 11:53:05 +0800

    test git v3.0

--- src/main/java/com/testgit/gittest/GittestApplication.java
+++ src/main/java/com/testgit/gittest/GittestApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @version  git2.0
+ * @version  git3.0
  *
  */
 @SpringBootApplication
