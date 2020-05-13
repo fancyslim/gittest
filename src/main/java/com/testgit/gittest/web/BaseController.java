0af874a4fb5eecd50b962f0369753ba69fbb50c8
wufan <729627242@qq.com>
2020-05-13 10:20:03 +0800

    commit test

---
+++ src/main/java/com/testgit/gittest/web/BaseController.java
@@ -0,0 +1,13 @@
+package com.testgit.gittest.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 吴凡
+ * @date 2020/5/12 21:20
+ */
+@RestController
+public class BaseController {
+
+
+}
