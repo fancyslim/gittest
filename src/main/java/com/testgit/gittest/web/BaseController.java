35eb24f7da2c502160e0def5f5f3a83967058b6e
wufan <729627242@qq.com>
2020-05-13 10:43:58 +0800

    changed base

--- src/main/java/com/testgit/gittest/web/BaseController.java
+++ src/main/java/com/testgit/gittest/web/BaseController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * git
- *
  * @author 吴凡
  * @date 2020/5/12 21:20
  */
