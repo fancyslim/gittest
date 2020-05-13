39b5b36c3a43f0d45c538577caf6e983a2d5ffa6
wufan <729627242@qq.com>
2020-05-13 10:26:05 +0800

    commit index

--- src/main/java/com/testgit/gittest/web/BaseController.java
+++ src/main/java/com/testgit/gittest/web/BaseController.java
@@ -1,13 +1,26 @@
 package com.testgit.gittest.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * git
+ *
  * @author 吴凡
  * @date 2020/5/12 21:20
  */
 @RestController
 public class BaseController {
 
+    @GetMapping("/index")
+    public ResponseEntity<String> getService() {
+        return new ResponseEntity("<h1>123</h1>", HttpStatus.OK);
+    }
 
+    @GetMapping("/test")
+    public ResponseEntity<String> testService() {
+        return new ResponseEntity("<h1>hello</h1>", HttpStatus.OK);
+    }
 }
